acf2908d693709ecacc047cd5d2a364e5692b8f1
Actek <ATK@hotmail.com>
2021-05-25 13:33:24 -0500

    Add project files.

---
+++ PostgrestCloud/Controllers/WeatherForecastController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using PostgrestCloud.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PostgrestCloud.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+
+        private readonly MyWebApiContext _context;
+        public WeatherForecastController(MyWebApiContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/authors
+        public IEnumerable<Group> Get()
+        {
+            return _context.Groups.ToList();
+        }
+
+        // GET api/authors/5
+        [HttpGet("{id}")]
+        public Group Get(int id)
+        {
+            return _context.Groups.FirstOrDefault(x => x.Id == id);
+        }
+
+        // POST api/authors
+        [HttpPost]
+        public IActionResult Post([FromBody] Group value)
+        {
+            _context.Groups.Add(value);
+            _context.SaveChanges();
+            return StatusCode(201, value);
+        }
+    }
+}
---
+++ PostgrestCloud/Models/MyWebApiContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace PostgrestCloud.Models
+{
+    public class MyWebApiContext : DbContext
+    {
+        public MyWebApiContext(DbContextOptions<MyWebApiContext> options) : base(options) { }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Group> Groups { get; set; }
+    }
+}
